9e4f7c9c376ba9384015f29ea97304831b619ba0
shashank <shashankshe2810@gmail.com>
2026-04-07 03:40:59 +0530

    Use localStorage for backend URL configuration

--- extension/background.js
+++ extension/background.js
@@ -7,7 +7,8 @@
  * Access rules. Service workers are exempt from that restriction.
  */
 
-const BACKEND = "https://vigil.up.railway.app";
+const BACKEND = localStorage.getItem("VIGIL_BACKEND") 
+  || "https://vigil.up.railway.app";
 
 // Set default badge on install
 chrome.runtime.onInstalled.addListener(() => {
